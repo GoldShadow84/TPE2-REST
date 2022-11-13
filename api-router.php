019e006f01a2522b3907495dae068d4f22ef6aa1
goldshadow84 <javierse9@gmail.com>
2022-11-13 10:56:11 -0300

    comenzando autenticacion2

--- api-router.php
+++ api-router.php
@@ -2,6 +2,7 @@
 
 require_once './libs/Router.php';
 require_once './app/controllers/review-api.controller.php';
+require_once './app/controllers/auth-api.controller.php';
 
 //creacion del router
 
@@ -15,6 +16,7 @@ $router->addRoute('reviews/:ID', 'DELETE', 'ReviewApiController', 'deleteReview'
 $router->addRoute('reviews', 'POST', 'ReviewApiController', 'insertReview'); 
 $router->addRoute('reviews/:ID', 'PUT', 'ReviewApiController', 'updateReview'); 
 
+$router->addRoute("auth/token", 'GET', 'AuthApiController', 'getToken');
 
 //ejecuta la ruta, sea la que sea
 
